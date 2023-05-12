d529e24535d8f0e78065976888c351808cd409ca
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-05-12 18:15:13 +0200

    created a rourte for "/" with usage instructions

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require("express");
 const http = require("https");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>Usage:</h1> <br/><h2>https://book-analyzer.abdelrahmen1.repl.co/[your-isbn-number-here]</h2>"
+  );
+});
 app.get("/:isbn", (req, res) => {
   http.get(
     `https://openlibrary.org/isbn/${req.params.isbn}.json`,
